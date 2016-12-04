79b9ccea3dfe28d25d432bcbf32b996944841b6b
Julien Lebunetel <julien@lebunetel.com>
2016-12-04 21:47:55 +0100

    création et activation des modèles

--- graph/livre/models.py
+++ graph/livre/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Node(models.Model):
+    entity = models.CharField(max_length=200)
+    value = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+
+
+class Edge(models.Model):
+    start_node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='start_node')
+    end_node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='end_node')
+    action = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
